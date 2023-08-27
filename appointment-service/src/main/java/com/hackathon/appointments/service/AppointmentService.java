5d463bc325cf4bfa373360161bb28564501cb75d
krish420 <hatikrishnendu135@gmail.com>
2023-08-27 11:47:07 +0000

    doctor list bug fix

--- appointment-service/src/main/java/com/hackathon/appointments/service/AppointmentService.java
+++ appointment-service/src/main/java/com/hackathon/appointments/service/AppointmentService.java
@@ -40,82 +40,91 @@ public class AppointmentService {
     @Value("${user-management-service.base-url}")
     private String urlOfUserManagementService;
 
-    public Map<List<String>, Map<LocalDate, Map<LocalTime,String>>>  availableSlots(LocalDate fromDate, LocalDate toDate) {
-        Map<List<String>, Map<LocalDate, Map<LocalTime,String>>> slotsWithDoctors = new HashMap<>();
-        Map<LocalDate, Map<LocalTime,String>> allSlots = new TreeMap<>();
-
+    public Map<List<String>, Map<LocalDate, Map<LocalTime, String>>> availableSlots(LocalDate fromDate,
+            LocalDate toDate) {
+        Map<List<String>, Map<LocalDate, Map<LocalTime, String>>> slotsWithDoctors = new HashMap<>();
+        Map<LocalDate, Map<LocalTime, String>> allSlots = new TreeMap<>();
 
         for (LocalDate date = fromDate; !date.isAfter(toDate); date = date.plusDays(1)) {
             List<LocalTime> bookedSlots = bookedSlotForAParticularDate(date);
 
-            LocalTime startTime = LocalTime.of(9,0);
-            LocalTime endTime = LocalTime.of(16,0);
+            LocalTime startTime = LocalTime.of(9, 0);
+            LocalTime endTime = LocalTime.of(16, 0);
             int duration = 30;
 
             LocalTime currentTime = startTime;
-            TreeMap<LocalTime,String> slotWithAvailability = new TreeMap<>();
-            while (currentTime.isBefore(endTime)){
-                slotWithAvailability.put(currentTime,"available");
+            TreeMap<LocalTime, String> slotWithAvailability = new TreeMap<>();
+            while (currentTime.isBefore(endTime)) {
+                slotWithAvailability.put(currentTime, "available");
                 currentTime = currentTime.plusMinutes(duration);
             }
             // traverse the map and set already available date in slot to booked
-            for(LocalTime slot : bookedSlots){
-                if(slotWithAvailability.containsKey(slot)) {
+            for (LocalTime slot : bookedSlots) {
+                if (slotWithAvailability.containsKey(slot)) {
                     slotWithAvailability.remove(slot);
-//                    slotWithAvailability.put(slot,"booked");
+                    // slotWithAvailability.put(slot,"booked");
                 }
             }
-            allSlots.put(date,slotWithAvailability);
+            allSlots.put(date, slotWithAvailability);
 
         }
         List<String> doctors = makeACallToUserManagementService();
-        slotsWithDoctors.put(doctors,allSlots);
+        slotsWithDoctors.put(doctors, allSlots);
         return slotsWithDoctors;
 
     }
 
-
     public List<Appointment> fetchForAParticularPatient(String patientId) {
         List<Appointment> appointmentList = appointmentRepository.findByPatientId(patientId);
-        if(appointmentList.isEmpty()) throw new ResourceNotFoundException("Patient with id: "+patientId+" not found!");
+        if (appointmentList.isEmpty())
+            throw new ResourceNotFoundException("Patient with id: " + patientId + " not found!");
         return appointmentList;
     }
 
     public Appointment book(Appointment appointment) {
-        if(appointmentRepository.existsByAppointmentDateAndAppointmentTime(appointment.getAppointmentDate(),appointment.getAppointmentTime()))
-            throw new DuplicateEntryException("Slot: "+appointment.getAppointmentTime()+" not available for date: "+appointment.getAppointmentDate());
+        if (appointmentRepository.existsByAppointmentDateAndAppointmentTime(appointment.getAppointmentDate(),
+                appointment.getAppointmentTime()))
+            throw new DuplicateEntryException("Slot: " + appointment.getAppointmentTime() + " not available for date: "
+                    + appointment.getAppointmentDate());
         Appointment savedAppointment = appointmentRepository.save(appointment);
         makeACallToNotificationService(savedAppointment);
         log.info("Appointment booked successfully!");
         return savedAppointment;
     }
 
-
-    private List<LocalTime> bookedSlotForAParticularDate(LocalDate date){
+    private List<LocalTime> bookedSlotForAParticularDate(LocalDate date) {
         List<LocalTime> bookedSlots = new ArrayList<>();
         List<Appointment> bookedAppointmentListForProvidedDate = appointmentRepository.findByAppointmentDate(date);
-        for (Appointment appointment: bookedAppointmentListForProvidedDate){
+        for (Appointment appointment : bookedAppointmentListForProvidedDate) {
             bookedSlots.add(appointment.getAppointmentTime());
         }
         return bookedSlots;
     }
-    private void makeACallToNotificationService(Appointment appointment){
+
+    private void makeACallToNotificationService(Appointment appointment) {
         // make a http call to entity-manager
-        String uri = urlOfNotificationManagementService+"/api/v1/schedule/notify";
-        String addedEmployeeFromEntityManagerService =
-                restTemplate.postForEntity(uri,appointment,String.class).getBody();
-        log.info("Notification Send to email id: "+appointment.getPatientEmail());
+        String uri = urlOfNotificationManagementService + "/api/v1/schedule/notify";
+        String addedEmployeeFromEntityManagerService = restTemplate.postForEntity(uri, appointment, String.class)
+                .getBody();
+        log.info("Notification Send to email id: " + appointment.getPatientEmail());
     }
 
-    private List<String> makeACallToUserManagementService(){
+    private List<String> makeACallToUserManagementService() {
         // String uri = urlOfUserManagementService+"/api/v1/actor/doctors";
         List<String> doctorList = new ArrayList<>();
-        for(Doctor d : userManagementRepository.getDoctors().getDoctors()){
-           String str = "[ Doctor: "+d.getDoctorName()+" ,Specialization: "+d.getSpecialization()+" ]";
-           doctorList.add(str);
+
+        List<Doctor> fetchedDoctorList = userManagementRepository.getDoctors().getDoctors();
+        if (fetchedDoctorList.isEmpty()) {
+            doctorList.add("No Doctors available");
+        } else {
+            for (Doctor d : fetchedDoctorList) {
+                String str = "[ Doctor: " + d.getDoctorName() + " ,Specialization: " +
+                        d.getSpecialization() + " ]";
+                doctorList.add(str);
+            }
         }
-        // System.out.println(doctorList);
-        // return  userManagementRepository.getDoctors().getDoctors();
+
+        // return userManagementRepository.getDoctors().getDoctors();
         return doctorList;
     }
 }
