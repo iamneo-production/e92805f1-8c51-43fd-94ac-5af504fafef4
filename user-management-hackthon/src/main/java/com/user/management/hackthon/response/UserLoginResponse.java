4fe4d56c97260bd9e2b4a1ef4b34f5fa3b5fec3c
ksreeram09 <sreeram.kurva99@gmail.com>
2023-08-27 14:05:03 +0530

    not null response handled in json response

--- user-management-hackthon/src/main/java/com/user/management/hackthon/response/UserLoginResponse.java
+++ user-management-hackthon/src/main/java/com/user/management/hackthon/response/UserLoginResponse.java
@@ -3,8 +3,12 @@ package com.user.management.hackthon.response;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 public class UserLoginResponse {
 	
+	@JsonInclude(Include.NON_NULL)
 	private Map<String, String> respnose = new HashMap<>();
 	
 	public Map<String, String> getRespnose() {
