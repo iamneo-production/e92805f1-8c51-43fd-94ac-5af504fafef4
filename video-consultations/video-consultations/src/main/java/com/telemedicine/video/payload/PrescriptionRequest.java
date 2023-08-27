2e1b0d32745863853b87683910fa2932d02d2311
imsurajmourya <imsurajmourya@gmail.com>
2023-08-27 06:05:26 +0000

    Video Consultation

---
+++ video-consultations/video-consultations/src/main/java/com/telemedicine/video/payload/PrescriptionRequest.java
@@ -0,0 +1,13 @@
+package com.telemedicine.video.payload;
+
+public class PrescriptionRequest {
+    private String medicines;
+
+    public String getMedicines() {
+        return medicines;
+    }
+
+    public void setMedicines(String medicines) {
+        this.medicines = medicines;
+    }
+}
