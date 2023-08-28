bff41c57cdb7cd29b38dc2ae354575a12d8219e1
imsurajmourya <imsurajmourya@gmail.com>
2023-08-28 05:02:56 +0000

    new

--- video-consultations/video-consultations/src/main/java/com/telemedicine/video/service/VideoConsultationService.java
+++ video-consultations/video-consultations/src/main/java/com/telemedicine/video/service/VideoConsultationService.java
@@ -1,5 +1,4 @@
 package com.telemedicine.video.service;
-
 import com.telemedicine.video.entity.Consultation;
 import com.telemedicine.video.entity.Prescription;
 import com.telemedicine.video.exception.ResourceNotFoundException;
@@ -8,6 +7,7 @@ import com.telemedicine.video.repository.PrescriptionRepository;
 import com.telemedicine.video.repository.VideoConsultationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -20,7 +20,19 @@ public class VideoConsultationService {
 
     @Autowired
     private PrescriptionRepository prescriptionRepository;
+    private final String CONSULTATION_SERVICE_URL = "http://APPOINTMENT-SERVICE/api/v1/appointment/history";
 
+    private final RestTemplate restTemplate;
+
+    public VideoConsultationService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public Consultation getConsultationData(String consultationId) {
+        String url = CONSULTATION_SERVICE_URL + "/" + consultationId;
+
+        return restTemplate.getForObject(url, Consultation.class);
+    }
     public Consultation getVideoConsultation(String patientId) {
         return consultationRepository.findByPatientId(patientId)
                 .orElseThrow(() -> new ResourceNotFoundException("Video consultation not found for patient: " + patientId));
